26fe26293fafb5ecb1eada69e666538e4d1c944d
Ghastly2001 <85193542+Ghastly2001@users.noreply.github.com>
2021-06-23 17:06:08 +0530

    Add files via upload

---
+++ Text_to_handwriting_red.py
@@ -0,0 +1,5 @@
+import pywhatkit as kit
+
+kit.text_to_handwriting("heya !!.\nIt's your friend Ghastly here its my another git repo. which might help you to complete your assignments easily.\nThough there are few restrictions here but i'll be surely updating this in future", rgb=[ 255, 0, 0])
+
+# You can set any ink colour by searching RGB values from the google.
